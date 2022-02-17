99c45832e604fd996525f1578dae7726994e6b37
fujiiree120 <fujiiree120@gmail.com>
2022-02-17 20:42:16 +0900

    組み込み

--- main.tf
+++ main.tf
@@ -2,9 +2,18 @@ variable = "example_instance_type"{
     default = "t3.micro"
 }
 
-resourse "aws_instance" "example"{
+resource "aws_instance" "example"{
     ami = "ami-03cfd0f5d33134a76"
     instance_type = var.example_instance_type
+    user_data = file("./user_data.sh")
+}
+
+//apply時の出力
+output "example_instance_id" {
+  value       = aws_instance_id
+  sensitive   = true
+  description = "description"
+  depends_on  = []
 }
 
 
