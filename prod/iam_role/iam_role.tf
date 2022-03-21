7193c1d9f73060538d2da212b75ca3f40de8800f
fujiiree120 <fujiiree120@gmail.com>
2022-03-21 16:38:13 +0900

    ecs

--- prod/iam_role/iam_role.tf
+++ prod/iam_role/iam_role.tf
@@ -28,6 +28,13 @@ resource "aws_iam_role_policy_attachment" "default" {
   policy_arn = aws_iam_policy.default.arn
 }
 
+module "ecs_task_execution_role"{
+  source = "./iam_role"
+  name = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
 output "iam_role_arn" {
   value       = aws_iam_role.default.arn
 }
